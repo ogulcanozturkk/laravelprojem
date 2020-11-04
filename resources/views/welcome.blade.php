49b2423154d68997d5f1fad1261b88d70435a8be
ogulcanozturkk <ogulcanozturkk@hotmail.com>
2020-11-04 11:00:27 +0300

    gonderim islemi

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends ('layouts.app')
+@section('content')
+
+    <div class="jumbotron">
+        <h1>Laravel fotograf galerisine hos geldiniz</h1>
+        <p>Devam etmek ve fotograf yuklemek icin uye olmalisiniz</p>
+
+    </div>
+
+@endsection
